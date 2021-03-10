b320620b3fdad108a36dc67dbc03e718cef2bc75
Lenoir Christopher <lenoirc.fr@gmail.com>
2021-03-10 14:08:11 +0100

    get fleet movements / get planet list and select one

--- src/Api/game/navigation.ts
+++ src/Api/game/navigation.ts
@@ -1,32 +1,44 @@
-import { Page } from "puppeteer";
+import { Page } from 'puppeteer';
 
 export class Navigation {
-    serverUrl: string;
-    constructor(serverUrl: string) {
-        this.serverUrl = serverUrl;
-    }
+  serverUrl: string;
+  constructor(serverUrl: string) {
+    this.serverUrl = serverUrl;
+  }
 
-    async goToHomePage(page: Page) {
-        await page.goto(`${this.serverUrl}/game`);
-    }
+  async goToHomePage(page: Page) {
+    await page.goto(`${this.serverUrl}/game`);
+  }
 
-    async goToResourcePage(page: Page) {
-        await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=supplies`);
-    }
+  async goToResourcePage(page: Page) {
+    await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=supplies`);
+  }
 
-    async goToFacilitiesPage(page: Page) {
-        await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=facilities`);
-    }
+  async goToFacilitiesPage(page: Page) {
+    await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=facilities`);
+  }
 
-    async goToResearchPage(page: Page) {
-        await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=research`);
-    }
+  async goToResearchPage(page: Page) {
+    await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=research`);
+  }
 
-    async goToShipPage(page: Page) {
-        await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=shipyard`);
-    }
+  async goToShipPage(page: Page) {
+    await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=shipyard`);
+  }
 
-    async goToDefensePage(page: Page) {
-        await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=defenses`);
-    }
+  async goToDefensePage(page: Page) {
+    await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=defenses`);
+  }
+
+  async goToFleetMovementsPage(page: Page) {
+    await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=movement`);
+  }
+
+  async goToOverviewPage(page: Page) {
+    await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=overview`);
+  }
+
+  async goToPlanetOverviewPage(page: Page, planetId: string) {
+    await page.goto(`${this.serverUrl}/game/index.php?page=ingame&component=overview&cp=${planetId}`);
+  }
 }
